a4d06bd599b14a753b6256b77f5bf1a4c1e7a1dc
EmillySatiro <sousasatiroclaraemilly@gmail.com>
2024-04-18 22:16:29 -0300

    ett

--- funcoes/cliente/cadastro.h
+++ funcoes/cliente/cadastro.h
@@ -19,14 +19,16 @@ void cadastrarCliente(CLIENTE *cliente,int *i){
 
 void mostrarclientes(int qtdc, CLIENTE * cliente){
     
+    printf("----------------------------------------------\n");
     printf("Lista de Clientes: \n");
-
+    printf("----------------------------------------------\n");
     for(int i = 0; i < qtdc; i++){
         printf("Cliente %d: \n",i + 1);
         printf(" ID: %d\n",cliente[i].pessoa->id);
         printf(" Nome: %s\n",cliente[i].pessoa->nome);
         printf(" Idade: %d\n",cliente[i].pessoa->idade);
         printf(" Limite de credito: %.2f\n",cliente[i].limite_de_credito);
+        printf("----------------------------------------------\n");
         printf("\n");
     }
 }
